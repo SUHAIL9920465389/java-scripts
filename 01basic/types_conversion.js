84a9da4a35b030e6d9bbbecce7b5a04efafcc31f
SUHAIL9920465389 <139313355+SUHAIL9920465389@users.noreply.github.com>
2024-03-05 19:02:32 +0000

    container config done

---
+++ 01basic/types_conversion.js
@@ -0,0 +1,28 @@
+// let score = "hitesh"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+ console.log(typeof stringNumber);
+ //***********************operation*********************
+//  let value = 3
+//  let negValue = -value
+//  console.log(negValue);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 1);
+console.log(undefined > 1);
+console.log(undefined < 1);
+console.log("2" === 2);
